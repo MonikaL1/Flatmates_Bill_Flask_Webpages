fe37c48fa743266dd15404803a2d2e8c20e8f2f0
Monika <lumimonika1@gmail.com>
2024-05-15 20:43:41 -1100

    Processing the User Data Completing the Results page

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask.views import MethodView
 from wtforms import Form, StringField, SubmitField
 from flask import Flask, render_template, request
+from flatmates_bill import flat
 
 app = Flask(__name__)
 
@@ -19,8 +20,15 @@ class BillFormPage(MethodView):
 class ResultsPage(MethodView):
     def post(self):
         billform = BillForm(request.form)
-        amount = billform.amount.data
-        return amount
+
+        the_bill = flat.Bill(float(billform.amount.data), billform.period.data)
+        flatmate1 = flat.Flatmate(billform.name1.data, float(billform.days_in_house1.data))
+        flatmate2 = flat.Flatmate(billform.name2.data, float(billform.days_in_house2.data))
+
+        return render_template('results.html', name1=flatmate1.name,
+                               amount1=flatmate1.pays(the_bill, flatmate2),
+                               name2=flatmate2.name,
+                               amount2=flatmate2.pays(the_bill, flatmate1))
 
 
 class BillForm(Form):
