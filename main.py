3247e4f2c901b6d4cb2d22432253ec89dfa673a1
Monika <lumimonika1@gmail.com>
2024-05-15 20:15:58 -1100

    Implementing the form submit button

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask.views import MethodView
-from wtforms import Form
-from flask import Flask, render_template
+from wtforms import Form, StringField, SubmitField
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -12,18 +12,32 @@ class HomePage(MethodView):
 
 class BillFormPage(MethodView):
     def get(self):
-        return render_template('bill_form_page.html')
+        bill_form = BillForm()
+        return render_template('bill_form_page.html', billform=bill_form)
 
 
 class ResultsPage(MethodView):
-    pass
+    def post(self):
+        billform = BillForm(request.form)
+        amount = billform.amount.data
+        return amount
 
 
 class BillForm(Form):
-    pass
+    amount = StringField("Bill Amount: ")
+    period = StringField("Bill Period: ")
+
+    name1 = StringField("Name: ")
+    days_in_house1 = StringField("Days in the house: ")
+
+    name2 = StringField("Name: ")
+    days_in_house2 = StringField("Days in the house: ")
+
+    button = SubmitField("Calculate")
 
 
 app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
 app.add_url_rule('/bill_form', view_func=BillFormPage.as_view('bill_form_page'))
+app.add_url_rule('/results', view_func=ResultsPage.as_view('results_page'))
 
 app.run(debug=True)
